21d457d6e65fd721cd909c30c638f8c7768e320b
sahalux111 <sahalal77@gmail.com>
2024-10-09 01:29:37 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,9 @@
+# config.py
+
+# Configuration for MySQL connection (Hostinger)
+db_config = {
+    'host': 'your_hostinger_db_host',
+    'user': 'your_db_user',
+    'password': 'your_db_password',
+    'db': 'your_database_name'
+}
