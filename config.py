0889661c16e3b42e3bb097e4a180d3c93d12b9a6
sahalux111 <sahalal77@gmail.com>
2024-10-09 11:57:47 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,5 @@
+
+class Config:
+    SECRET_KEY = 'your_secret_key'
+    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://username:password@hostinger_db_url/database_name'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
