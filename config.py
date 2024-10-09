79b42fc3682d9d0bcf95b87d9f3b995430d28253
sahalux111 <sahalal77@gmail.com>
2024-10-09 11:54:26 +0530

    Delete config.py

--- config.py
+++
@@ -1,9 +0,0 @@
-# config.py
-
-# Configuration for MySQL connection (Hostinger)
-db_config = {
-    'host': 'srv1672.hstgr.io',
-    'user': 'u953503039_root',
-    'password': 'Radblox!1',
-    'db': 'u953503039_radschedule'
-}
